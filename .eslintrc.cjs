129bcdd0dfc29277257b69ad8e752a1a264fd08c
Lukas von Blarer <luk@attribute.ch>
2025-02-27 15:17:32 +0100

    fix: eslint issues

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  extends: ['@nuxt/eslint-config'],
+  ignorePatterns: [
+    'dist/**',
+    'node_modules/**',
+    '.git/**',
+    '.github/**'
+  ],
+  rules: {
+    // Add any custom rules here
+  }
+}
